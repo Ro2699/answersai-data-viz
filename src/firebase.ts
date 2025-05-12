04edb321540758c1ca159c00de43b5c2b657c99e
Ro2699 <rohith.damerla@gmail.com>
2025-05-12 16:47:45 -0500

    Fix: use 'selected' in VariablePanel and remove TS warnings

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD7gHmyISMlylTwGHoYEBCR23Kl1UUkLlg",
   authDomain: "answersai-data-viz-3f6ce.firebaseapp.com",
@@ -17,4 +13,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Auth instance (used across the app)
 export const auth = getAuth(app);
+
+
